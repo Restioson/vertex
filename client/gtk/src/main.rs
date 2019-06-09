2f192f1ab6aabc4912c67290f532af0a08a76046
Restioson <restiosondev@gmail.com>
2019-06-09 12:47:20 +0200

    Minor restructure

--- client/gtk/src/main.rs
+++ client/gtk/src/main.rs
@@ -1,16 +1,23 @@
+use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::Window;
+use gtk::{Application, ApplicationWindow};
 
 fn main() {
-    gtk::init().expect("Error initialising gtk");
+    let app = Application::new("com.github.restioson.vertex", Default::default())
+        .expect("Error initializing GTK application");
+
+    app.connect_activate(|app| create(app));
+    app.run(&std::env::args().collect::<Vec<_>>());
+}
+
+fn create(app: &Application) {
     let glade_src = include_str!("client.glade");
     let builder = gtk::Builder::new_from_string(glade_src);
 
-    let window: Window = builder.get_object("window").unwrap();
+    let window: ApplicationWindow = builder.get_object("window").unwrap();
+    window.set_application(app);
     window.set_title("Vertex client");
-    window.set_default_size(350, 70);
+    window.set_default_size(640, 480);
 
     window.show_all();
-
-    gtk::main();
 }
