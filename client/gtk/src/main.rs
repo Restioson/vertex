d3e0509d7cf68a26cb4a8e3b22fa0ca0c8e664e8
gegy1000 <gegy1000@gmail.com>
2019-06-11 14:55:36 +0200

    Use listbox for chat messages

--- client/gtk/src/main.rs
+++ client/gtk/src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Window, Entry, Label, ListBox, TextView};
+use gtk::{Window, Entry, Label, ListBox, TextView, ListBoxRow, Separator, Orientation};
 use relm::{Relm, Update, Widget, connect, connect_stream};
 use relm_derive::*;
 use url::Url;
@@ -44,12 +44,29 @@ impl Win {
     fn handle_action(&mut self, action: Action) -> () {
         match action {
             Action::AddMessage(msg) => {
-                let text_buffer = self.widgets.messages.get_buffer().unwrap();
-                let message = format!("{}: {}\n", msg.author, msg.content);
-                text_buffer.insert(&mut text_buffer.get_end_iter(), &message);
+                self.push_message(&msg.author, &msg.content);
             }
             Action::Error(_error) => {}
         }
+    }
+
+    fn push_message(&self, author: &str, content: &str) {
+        let row = ListBoxRow::new();
+        row.set_selectable(false);
+
+        let label = format!("{}: {}", author, content);
+        let label = Label::new(Some(label.as_str()));
+        label.set_xalign(0.0);
+
+        row.add(&label);
+
+        let separator = Separator::new(Orientation::Horizontal);
+
+        separator.show_all();
+        row.show_all();
+
+        self.widgets.messages.insert(&separator, -1);
+        self.widgets.messages.insert(&row, -1);
     }
 }
 
@@ -81,8 +98,6 @@ impl Update for Win {
                 self.model.room = Some(room);
             }
             VertexMsg::SendMessage(msg) => {
-                let text_buffer = self.widgets.messages.get_buffer().unwrap();
-
                 if msg.starts_with("/") {
                     let v: Vec<&str> = msg.splitn(2, ' ').collect();
 
@@ -91,28 +106,22 @@ impl Update for Win {
                             if v.len() == 2 {
                                 let room = RoomId(Uuid::parse_str(v[1]).expect("Invalid room id"));
                                 self.model.vertex.join_room(room).expect("Error joining room");
-                                text_buffer.insert(
-                                    &mut text_buffer.get_end_iter(),
-                                    &format!("Joined room {}\n", room.0),
-                                );
+                                self.push_message("Info", &format!("Joined room {}", room.0));
 
                                 self.model.room = Some(room);
                                 let txt: &str = &format!("#{}", room.0);
                                 let room_label = Label::new(Some(txt));
+                                room_label.set_xalign(0.0);
                                 self.widgets.rooms.insert(&room_label, -1);
                                 self.model.room_list.push(room);
                                 room_label.show_all();
                             } else {
-                                text_buffer.insert(&mut text_buffer.get_end_iter(), "Room id required");
+                                self.push_message("Info", "Room id required");
                             }
                         }
                         "/createroom" => {
-                            text_buffer.insert(&mut text_buffer.get_end_iter(), "Creating room...\n");
                             let room = self.model.vertex.create_room().expect("Error creating room");
-                            text_buffer.insert(
-                                &mut text_buffer.get_end_iter(),
-                                &format!("Joined room {}\n", room.0),
-                            );
+                            self.push_message("Info", &format!("Joined room {}", room.0));
 
                             self.model.room = Some(room);
                             let txt: &str = &format!("#{}", room.0);
@@ -121,7 +130,7 @@ impl Update for Win {
                             self.model.room_list.push(room);
                             room_label.show_all();
                         }
-                        _ => text_buffer.insert(&mut text_buffer.get_end_iter(), "Unknown command\n"),
+                        _ => self.push_message("Info", "Unknown command"),
                     }
 
                     return;
@@ -133,8 +142,7 @@ impl Update for Win {
 
                 let name = self.model.vertex.username();
 
-                // TODO: Unify
-                text_buffer.insert(&mut text_buffer.get_end_iter(), &format!("{}: {}\n", name, msg));
+                self.push_message(&name, &msg);
             }
             VertexMsg::Lifecycle => {
                 if let Some(action) = self.model.vertex.handle() {
@@ -165,9 +173,9 @@ impl Widget for Win {
         let builder = gtk::Builder::new_from_string(GLADE_SRC);
 
         let window: Window = builder.get_object("window").unwrap();
-        let messages: TextView = builder.get_object("messages").unwrap();
+        let messages: ListBox = builder.get_object("messages").unwrap();
         let entry: Entry = builder.get_object("message_entry").unwrap();
-        let rooms: ListBox = builder.get_object("channels").unwrap();
+        let rooms: ListBox = builder.get_object("rooms").unwrap();
 
         connect!(relm, window, connect_delete_event(_, _), return (Some(VertexMsg::Quit), Inhibit(false)));
         connect!(relm, rooms, connect_row_selected(_, row), {
@@ -198,7 +206,7 @@ impl Widget for Win {
 }
 
 struct Widgets {
-    messages: TextView,
+    messages: ListBox,
     entry: Entry,
     rooms: ListBox,
 }
